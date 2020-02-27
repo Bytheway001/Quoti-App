b10d78354299cf0b299ab1a1d81a961790794bed
Rafael <Bytheway0708@gmail.com>
2020-02-27 12:58:41 -0400

    Finshed Home Page View

--- Screens/Home/Home.js
+++ Screens/Home/Home.js
@@ -2,105 +2,67 @@ import React from 'react';
 import { View, Text, Button, Image, ImageBackground } from 'react-native';
 import { Container, Row, Col } from '../../Components/Layout';
 import { Colors } from '../../assets/Colors';
+import QuoteIcon from '../../assets/placeHolder.png';
 
+const HomeIcon = ({ src, title, style,borders }) => {
+    let borderRightWidth = borders['right']==true?2:0
+    let borderLeftWidth = borders['left']==true?2:0
+    let borderBottomWidth = borders['bottom']==true?2:0
+    let borderTopWidth = borders['top']==true?2:0
+    console.log(borderRightWidth)
+    
+    const styles = {
+        alignItems: 'center',
+        borderBottomWidth,
+        borderTopWidth,
+        borderLeftWidth,
+        borderRightWidth,
+        borderColor:Colors.lightBlue,
+        borderRightColor: Colors.lightBlue,
+        alignItems:'center',
+        justifyContent:'space-evenly',
+        paddingVertical:5
+    }
+    return (
+        <Row style={styles} >
+            <View>
+                <Image
+                    style={{ width: 60, height: 60 }}
+                    source={src}
+                >
+                </Image>
+
+            </View>
+
+
+            <View>
+                <Text style={{ marginLeft: 7, marginRight: 7 }}>{title}</Text>
+            </View>
+        </Row >
+    )
+}
 
 export const HomeScreen = (props) => {
     return (
-
         <ImageBackground
             style={{ width: '100%', height: '100%' }}
             source={require('../../assets/bgMerged.png')}
             resizeMode='cover'
         >
-
-
-
-
-
             <Container style={{ alignItems: 'center', justifyContent: 'center', paddingBottom: 200 }}>
-            <Image
-                style={{ width: 200, height: 200, display: "flex", alignItems: 'center', justifyContent: 'center' }}
-                resizeMode="contain"
-                source={require('../../assets/quoti-logo.png')}
-            ></Image>
-
-
+                <Image style={{ width: 200, height: 200, display: "flex", alignItems: 'center', justifyContent: 'center' }} resizeMode="contain" source={require('../../assets/quoti-logo.png')}></Image>
                 <Row>
-                    <Col>
-                        <Row style={{ alignItems: 'center', borderBottomWidth: 2, borderRightWidth: 2, borderBottomColor: Colors.lightBlue, borderRightColor: Colors.lightBlue }}>
-
-                            <View >
-                                <Image
-                                    style={{ width: 60, height: 60}}
-
-                                    source={require('../../assets/placeHolder.png')}
-                                >
-                                </Image>
-
-                            </View>
-
-
-                            <View>
-                                <Text style={{ marginLeft: 7, marginRight:7 }}>Comparador</Text>
-                            </View>
-
-                        </Row>
-                    </Col>
-
-                    <Row>
-                        <Col>
-                        <Row style={{ alignItems: 'center', borderBottomWidth: 2, borderBottomColor: Colors.lightBlue }}>
-                            <View>
-                                <Image
-                                    style={{ width: 60, height: 60, paddingLeft:10}}
-                                    resizeMode="contain"
-                                    source={require('../../assets/placeHolder.png')}
-                                >
-                                </Image>
-                            </View>
-                            <View>
-                                <Text style={{ marginLeft: 7, marginRight: 0 }}>Documentos</Text>
-                            </View>
-                            </Row>
-                        </Col>
+                    <Col size={1}>
+                        <HomeIcon src={QuoteIcon} title='Cotizador' borders={{right:true,bottom:true}}/>
+                        <HomeIcon src={QuoteIcon} title='Cotizador' borders={{bottom:true,right:true}}/>
                         
-                    </Row>
+                    </Col>
+                    <Col size={1}>
+                    <HomeIcon src={QuoteIcon} title='Cotizador' borders={{bottom:true}}/>
+                        <HomeIcon src={QuoteIcon} title='Cotizador' borders={{bottom:true}}/>
+                    </Col>
                 </Row>
 
-                <Row>
-                    <Col >
-                        <Row style={{ alignItems: 'center', borderBottomWidth: 2, borderRightWidth: 2, borderBottomColor: Colors.lightBlue, borderRightColor: Colors.lightBlue }}>
-                            <View style={{paddingLeft:5}} >
-                                <Image
-                                    style={{ width: 60, height: 60 }}
-                                    resizeMode="contain"
-                                    source={require('../../assets/placeHolder.png')}
-                                >
-                                </Image>
-                            </View>
-                            <View>
-                                <Text style={{ marginLeft: 20, marginRight:10 }}>Cotizador</Text>
-                            </View>
-                        </Row>
-                    </Col>
-                    <Row>
-                        <Col>
-                        <Row  style={{  alignItems: 'center',borderBottomWidth: 2, borderBottomColor: Colors.lightBlue}}>
-                            <View>
-                                <Image
-                                    style={{ width: 60, height: 60}}
-                                    resizeMode="contain"
-                                    source={require('../../assets/placeHolder.png')}
-                                >
-                                </Image>
-                            </View>
-                            <View>
-                                <Text style={{ marginLeft: 20, marginRight:10 }}>Soporte</Text>
-                            </View>
-                            </Row>
-                        </Col>
-                    </Row>
-                </Row>
 
             </Container>
         </ImageBackground>
