5ad45e03573ccd0b77293bec10b67452fd67a82c
Eduardo Urzua <EduardoUrzuaBo@gmail.com>
2020-03-03 18:41:39 -0400

    Adding Icon to the directorio dropdown
    Adding Rows and Cols to the text comp. inside the File.js

--- Screens/Directorio/Dropdown.js
+++ Screens/Directorio/Dropdown.js
@@ -2,27 +2,39 @@ import React, { Component, useState } from 'react';
 import { View, Text } from 'react-native';
 import { TouchableWithoutFeedback } from 'react-native-gesture-handler';
 import { Colors } from '../../assets/Colors';
-
+import { FontAwesomeIcon } from '@fortawesome/react-native-fontawesome'
+import { faAngleDoubleRight, faAngleRight } from '@fortawesome/free-solid-svg-icons'
+import { Row, Col } from '../../Components/Layout';
 
 
 export const DropDown = (props) => {
 
 
     return (
-        <View>
-            <TouchableWithoutFeedback onPress={() => props.onPress(props.for)} style={{  padding: 15  }}
+        <View >
+
+            <TouchableWithoutFeedback onPress={() => props.onPress(props.for)} style={{ padding: 15 }}
 
             >
-                <Text style={{ color:Colors.lightBlue , fontSize: 33 }}>{props.title}</Text>
-
+                <Row style={{ alignItems: 'center'}}>
+                    <Col>
+                        <FontAwesomeIcon icon={faAngleDoubleRight} color={Colors.lightBlue} size={20} />
+                    </Col>
+                    <Col>
+                        <Text style={{ color: Colors.lightBlue, fontSize: 33 }}>{props.title}</Text>
+                    </Col>
+                </Row>
             </TouchableWithoutFeedback>
 
-            <View style={{ boroderWidth: 1, borderColor: 'red', marginTop: -1, display: props.show ? 'flex' : 'none' }}>
-                <Text style={{ paddingLeft: 50 , fontSize:25, color:Colors.lightBlue}}>item1</Text>
-                <Text style={{ paddingLeft: 50 , fontSize:25, color:Colors.lightBlue}}>item2</Text>
-                <Text style={{ paddingLeft: 50 , fontSize:25, color:Colors.lightBlue}}>item3</Text>
-                <Text style={{ paddingLeft: 50 , fontSize:25, color:Colors.lightBlue}}>item4</Text>
-                <Text style={{ paddingLeft: 50 , fontSize:25, color:Colors.lightBlue}}>item5</Text>
+            <View style={{ display: props.show ? 'flex' : 'none' }}>
+
+                <Text style={{ paddingBottom: 5, paddingLeft: 50, fontSize: 25, color: Colors.lightBlue }}>Beneficios</Text>
+                <Text style={{ paddingBottom: 5, paddingLeft: 50, fontSize: 26, color: Colors.lightBlue }}>Formularios</Text>
+                <Text style={{ paddingBottom: 5, paddingLeft: 50, fontSize: 26, color: Colors.lightBlue }}>Tarifarios</Text>
+                <Text style={{ paddingBottom: 5, paddingLeft: 50, fontSize: 26, color: Colors.lightBlue }}>Endosos</Text>
+                <Text style={{ paddingBottom: 5, paddingLeft: 50, fontSize: 26, color: Colors.lightBlue }}>Coberturas</Text>
+                <Text style={{ paddingBottom: 5, paddingLeft: 50, fontSize: 26, color: Colors.lightBlue }}>General</Text>
+
             </View>
         </View>
     )
--- Screens/Directorio/Files.js
+++ Screens/Directorio/Files.js
@@ -2,29 +2,43 @@ import React, { useState } from 'react';
 import { View, Text } from 'react-native';
 import { DropDown } from './Dropdown';
 import { Col, Row, } from '../../Components/Layout';
-
+import { FontAwesomeIcon } from '@fortawesome/react-native-fontawesome'
+import { faAngleDoubleRight } from '@fortawesome/free-solid-svg-icons'
+import { Colors } from '../../assets/Colors';
+import { Left } from 'native-base';
 
 export const FileScreen = props => {
     const [show, setShow] = useState("");
 
     return (
-        <View style={{alignItems: 'center', justifyContent: 'center' , paddingTop: 100 , paddingRight:120}}>
+        <View style={{ alignItems: 'center', justifyContent: 'center', paddingTop: 100, paddingRight: 110 }}>
+
+            <Col>
+
+                <DropDown title={'Best Doctors'} for='BD' show={show === 'BD'} onPress={setShow}  ></DropDown>
+
+            </Col>
 
 
-            <Col size={0}>
-                <DropDown title={'-Best Docters'} for='BD' show={show === 'BD'} onPress={setShow}  ></DropDown>
+
+            <Col  >
+                <DropDown title={'BUPA Salud'} for='BS' show={show === 'BS'} onPress={setShow}  ></DropDown>
             </Col>
-            <Col size={0}>
-                <DropDown title={'-BUPA Salud'} for='BS' show={show === 'BS'} onPress={setShow}  ></DropDown>
+
+
+            <Col >
+                <DropDown title={'VUMI Group'} for='VG' show={show === 'VG'} onPress={setShow}  ></DropDown>
             </Col>
-            <Col size={0}>
-                <DropDown title={'-VUMI Group'} for='VG'show={show === 'VG'} onPress={setShow}  ></DropDown>
+
+
+            <Col >
+                <DropDown title={'Allianz Care'} for='AC' show={show === 'AC'} onPress={setShow}  ></DropDown>
+
             </Col>
-            <Col size={0}>
-                <DropDown title={'-Allianz Care'} for='AC'show={show === 'AC'} onPress={setShow}  ></DropDown>
-            </Col>
-            <Col size={0}>
-                <DropDown title={'-BMI Seguros'}for='BM'  show={show === 'BM'} onPress={setShow}  ></DropDown>
+
+
+            <Col >
+                <DropDown title={'BMI Seguros'} for='BM' show={show === 'BM'} onPress={setShow}  ></DropDown>
             </Col>
 
 
